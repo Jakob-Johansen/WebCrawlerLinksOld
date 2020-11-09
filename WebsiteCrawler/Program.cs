744a880b3379f09cafca5f7156976ac3b47aabf2
Test Testsen <sadsafefs84fe6a4f5saf5s@outlook.dk>
2020-11-09 10:06:53 +0100

    Update

--- WebsiteCrawler/Program.cs
+++ WebsiteCrawler/Program.cs
@@ -40,11 +40,11 @@ namespace WebsiteCrawler
 
             // MÅSKE, GØR SÅ DEN CRAWLER FLERE SIDER PÅ EN GANG.
 
-            //TILFØJ ROBOTS:TXT READER OG XML READER TIL PROJEKTET.
+            // TILFØJ ROBOTS:TXT READER OG XML READER TIL PROJEKTET.
 
 
-            //Crawler crawler = new Crawler("http://monosoft.dk/");
-            //await crawler.LoadCrawler();
+            // Crawler crawler = new Crawler("http://monosoft.dk/");
+            // await crawler.LoadCrawler();
 
             Test test = new Test("https://www.automobile.tn/");
             await test.LoadCrawler();
